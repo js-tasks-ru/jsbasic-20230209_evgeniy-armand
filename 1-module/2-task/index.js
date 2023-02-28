f6759b716fa8ae99e21270c7e72937b1db0a6d6e
Евгений Арманд <igestam@gmail.com>
2023-02-28 14:01:48 +0300

    проверка имени

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,7 +10,8 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  // ваш код...
+  let successName = !!name && name.length > 3 && !(name.includes(' '));
+  return successName;
 }
 
 function sayHello() {
